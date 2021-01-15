95306c589a21bc213ff858a1b1edf408cac3a0a0
Jorge Vergara <jvc.9109@hotmail.com>
2021-01-15 22:32:35 +0100

    optimized version

--- src/day15.py
+++ src/day15.py
@@ -1,4 +1,26 @@
 
+def problem_optimized(numbers, max_turns):
+    turn = 1
+    spoken_numbers = {}
+    age = 0
+    firsts_numbers = len(numbers)
+
+    for index, number in enumerate(numbers):
+        spoken_numbers[number] = [index]
+    last_spoken_number = numbers[-1]
+    for turn in range(firsts_numbers, max_turns):
+        player_will_say = 0
+        said_numbers = spoken_numbers.keys()
+        if len(spoken_numbers[last_spoken_number]) > 1:
+            player_will_say = spoken_numbers[last_spoken_number][0] - spoken_numbers[last_spoken_number][1]
+        if len(spoken_numbers[last_spoken_number]) > 1000:
+            spoken_numbers[last_spoken_number] = spoken_numbers[last_spoken_number][:1]
+        if player_will_say in said_numbers:     
+            spoken_numbers[player_will_say].insert(0,turn)
+        else:
+            spoken_numbers[player_will_say] = [turn]
+        last_spoken_number = player_will_say
+    print(last_spoken_number)
 
 def problem_1(numbers, max_turns):
     turn = 1
@@ -11,10 +33,6 @@ def problem_1(numbers, max_turns):
             player_says = (turn, numbers[turn-1])
         else:
             turn_said_last_spoken, last_spoken_number = player_says
-            # for turn_said, spoken_number in spoken_numbers[1:]:
-            #     if last_spoken_number == spoken_number:
-            #         player_says = (turn, turn_said_last_spoken - turn_said)
-            #         break
             player_will_say = (turn, 0)
             said_numbers = spoken_numbers.keys()
             if last_spoken_number in said_numbers:
@@ -32,5 +50,5 @@ def problem_1(numbers, max_turns):
 
 with open("data/day15.txt") as file:
     starting_numbers = [int(number) for number in file.read().split(',')]
-    problem_1(starting_numbers, 30000000)
+    run(starting_numbers, 30000000)
     # problem_2(starting_numbers)
