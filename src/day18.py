f6d5a644ee4e598425e1117482e9fdd772d1ac50
Jorge Vergara <jvc.9109@hotmail.com>
2021-02-02 21:03:04 +0100

    solve day 18

--- src/day18.py
+++ src/day18.py
@@ -51,12 +51,44 @@ def calculate_value(operation):
             result = eval(f'{result}{action}{item}')
     return result
 
+
+def calc_operation(matchobj):
+    sum_operations = matchobj.group(0).replace('(', '').replace(')', '').split('*')
+    sum_results = []
+    for sum_operation in sum_operations:
+        sum_results.append(str(eval(sum_operation)))
+    
+    final_operation = '*'.join(sum_results)
+
+    return str(eval(final_operation))
+
+def calc_final(operation):
+    sum_operations = operation.split('*')
+    sum_results = []
+    for sum_operation in sum_operations:
+        sum_results.append(str(eval(sum_operation)))
+    
+    final_operation = '*'.join(sum_results)
+
+    return eval(final_operation)
+
+def problem2(operation):
+    pattern = r'\([^\(\)]+\)'
+    inner_operations = re.sub(pattern, calc_operation, operation)
+    if '(' in inner_operations:
+        inner_operations = str(problem2(inner_operations))
+    
+    return calc_final(inner_operations)
+
+
 with open('data/day18.txt') as file:
     operations = file.read().split('\n')
     total = 0
     for line in operations:
         line = line.replace(' ', '')
         result = calculate_value(line)
-        print(f'the result from {line} is {result}')
-        total += result
+        result2 = problem2(line)
+        print(f'the result from {line} is {result2}')
+        total += result2
+
     print(f'the final result is {total}')
